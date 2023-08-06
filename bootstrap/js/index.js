8b4eb2aea1fdd177b7666b696db0987bade42f74
unknown <momen07996@gmail.com>
2023-08-06 17:07:31 +0300

    update the project

---
+++ bootstrap/js/index.js
@@ -0,0 +1,25 @@
+let navbar_scroll = document.getElementById("navbar_scroll");
+
+window.onscroll = function () {
+  this.scrollY >= 100
+    ? (navbar_scroll.style.background =
+        "linear-gradient(155deg, rgb(30, 77, 141), rgb(56, 113, 166))")
+    : (navbar_scroll.style.background = "transparent");
+};
+
+var tag_header = document.querySelectorAll(".nav-link");
+for (let i = 0; i < tag_header.length; i++) {
+  var chekd = tag_header[i];
+  chekd.addEventListener("click", function () {
+    for (let i = 0; i < tag_header.length; i++) {
+      tag_header[i].classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel();
+});
+
+// AOS.init();
