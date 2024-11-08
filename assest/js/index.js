9011566de233632af2d39cf87b0eb60c2f41c85a
unknown <momen07996@gmail.com>
2024-11-08 15:33:37 +0200

    start tailwind in project

---
+++ assest/js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() >= 100) {
+      $("#navbarScroll").css(
+        "background",
+        "linear-gradient(155deg, rgb(30, 77, 141), rgb(56, 113, 166))"
+      );
+    } else {
+      $("#navbarScroll").css("background", "transparent");
+    }
+  });
+
+  $(".nav-link").on("click", function () {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  var bollen = true;
+  $("#divBackgroundColor").on("click", function () {
+    if (bollen) {
+      $("#diMenu1").css({
+        transform: "translateY(3px) rotate(45deg)",
+      });
+      $("#diMenu2").hide();
+      $("#diMenu3").css({
+        transform: "translateY(-3px) rotate(-45deg)",
+      });
+      $("#navbarSupportedContent").css("right", "0");
+      bollen = false;
+    } else {
+      $("#diMenu1").css("transform", "rotate(0deg)");
+      $("#diMenu2").show();
+      $("#diMenu3").css("transform", "rotate(0deg)");
+      $("#navbarSupportedContent").css("right", "-900px");
+      bollen = true;
+    }
+  });
+});
