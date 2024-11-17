8a9d42966e3e5779a44af5e2b8bc8fbadad2e7be
unknown <momen07996@gmail.com>
2024-11-17 16:14:47 +0200

    end the project and update the taiwlind

--- assest/js/index.js
+++ assest/js/index.js
@@ -12,4 +12,66 @@ $(document).ready(function () {
       $("#navbarScroll").css("background", "transparent");
     }
   });
+
+  const $items = $("[data-carousel-item]");
+  let currentIndex = 0;
+
+  function showItem(index) {
+    $items.each(function (i) {
+      if (i == index) {
+        $(this).removeClass("hidden").addClass("duration-200 ease-linear");
+      } else {
+        $(this).addClass("hidden");
+      }
+    });
+  }
+
+  showItem(currentIndex);
+
+  $("#prevBtn").on("click", function () {
+    currentIndex = (currentIndex - 1 + $items.length) % $items.length;
+    showItem(currentIndex);
+  });
+
+  $("#nextBtn").on("click", function () {
+    currentIndex = (currentIndex + 1) % $items.length;
+    showItem(currentIndex);
+  });
+
+  $("#owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      300: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
